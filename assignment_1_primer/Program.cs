116b75a2def672f2d51e7c215a3efdfd0152b1c4
Lenovo <pvk1@nemandi.ntv.is>
2022-11-21 10:35:36 +0000

    first commit

---
+++ assignment_1_primer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> numbers = new();
+
+            while (true)
+            {
+                Console.WriteLine("Please enter a number");
+
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("Highest value " + numbers.Max());
+                    Console.WriteLine("Lowest value " + numbers.Min());
+                    break;
+                }
+                else { numbers.Add(input); }
+
+
+
+            }
+
+        }
+
+    }
+}
+
+
